9fd929869bcddfa871285ac9ee95e08ea1b58618
Tirsa St. Fort <lifelovelori@gmail.com>
2023-02-15 17:02:52 -0800

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData (name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    })
+            .then((response) => response.json())
+            .then((data) => document.body.append(data.id))
+            .catch((error) => document.body.append(error.message))
+    
+}
+
+submitData()
